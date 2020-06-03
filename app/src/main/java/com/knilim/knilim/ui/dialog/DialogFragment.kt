032b8cc2800a58e6348ee42099d9e26f39f09523
loheagn <linan_123@buaa.edu.cn>
2020-06-03 19:18:07 +0800

    统一将chat重命名为dialog

--- app/src/main/java/com/knilim/knilim/ui/dialog/DialogFragment.kt
+++ app/src/main/java/com/knilim/knilim/ui/dialog/DialogFragment.kt
@@ -1,28 +1,26 @@
-package com.knilim.knilim.ui.chat
+package com.knilim.knilim.ui.dialog
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.knilim.knilim.R
 import com.knilim.knilim.ui.MainViewModel
 
-class ChatFragment : Fragment() {
+class DialogFragment : Fragment() {
 
-    private lateinit var chatViewModel: MainViewModel
+    private lateinit var dialogModel: MainViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        chatViewModel =
+        dialogModel =
             ViewModelProviders.of(this).get(MainViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_chat, container, false)
+        val root = inflater.inflate(R.layout.fragment_dialog, container, false)
         return root
     }
 }
