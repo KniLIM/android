e8cbb12503516519ac77de687a80a500b4adcbaf
loheagn <linan_123@buaa.edu.cn>
2020-06-05 00:17:38 +0800

    把通信模块改为Android Library, 方便打日志

--- communicate-server/src/main/java/com/knilim/knilim/server/MessageServer.java
+++ communicate-server/src/main/java/com/knilim/knilim/server/MessageServer.java
@@ -1,13 +1,43 @@
 package com.knilim.knilim.server;
 
+import android.util.Log;
+
 public class MessageServer {
+
+    private static String TAG = "socket-io";
+
+    /**
+     * 单例实现
+     */
+    private static volatile MessageServer INSTANCE = null;
+    public MessageServer getINSTANCE(String host, Integer port, MsgRcvListener msgRcvListener) {
+        if(INSTANCE == null) {
+            synchronized (MessageServer.class) {
+                if(INSTANCE == null) {
+                    INSTANCE = new MessageServer(host, port, msgRcvListener);
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+
     private String host;
     private Integer port;
     private MsgRcvListener msgRcvListener;
 
-    public MessageServer(String host, Integer port, MsgRcvListener msgRcvListener) {
+    private MessageServer(String host, Integer port, MsgRcvListener msgRcvListener) {
         this.host = host;
         this.port = port;
         this.msgRcvListener = msgRcvListener;
+    }
+
+    public boolean connect() {
+        try {
+            return true;
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+            return false;
+        }
     }
 }
--- communicate-server/src/main/java/com/knilim/knilim/server/MsgRcvListener.java
+++ communicate-server/src/main/java/com/knilim/knilim/server/MsgRcvListener.java
@@ -1,4 +1,9 @@
 package com.knilim.knilim.server;
 
 public interface MsgRcvListener {
+
+    /**
+     * 当socket server 监听到新消息的时候,应该回调该方法
+     */
+    void onMessageRcv();
 }
